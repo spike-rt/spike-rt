a7a3c91a2cf19605686dbeb07ce0ed2baf45653e
SHU Yoshifumi <envzhu@gmail.com>
2022-06-29 17:59:54 +0900

    refactor(drivers): fix build dependency in drivers/Makefile to make building time shorter.

---
+++ scripts/build-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -xe
+BUILD_DIR=${BUILD_DIR:-build}
+JOB_NUM=${JOB_NUM:-$(nproc)}
+
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+mkdir -p obj-primehub_kernel
+(cd obj-primehub_kernel && ../../asp3/configure.rb -T primehub_gcc -f)
+
+mkdir -p obj-primehub_test
+(cd obj-primehub_test && ../../asp3/configure.rb -T primehub_gcc -L ../obj-primehub_kernel -a ../../test/ -A test -m ../../common/Makefile.appl)
+
+(cd obj-primehub_kernel && make libkernel.a -j $JOB_NUM)
+(cd obj-primehub_test && make -j $JOB_NUM && make asp.bin -j $JOB_NUM)
+
+cd ..
