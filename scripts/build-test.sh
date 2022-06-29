ecdfa847e19c3242b154113685fdfe58d8e3df99
SHU Yoshifumi <envzhu@gmail.com>
2022-06-29 18:04:58 +0900

    fix(Makefile): use $(MAKE) instead of make when call make command inside Makefiles.

--- scripts/build-test.sh
+++ scripts/build-test.sh
@@ -12,6 +12,6 @@ mkdir -p obj-primehub_test
 (cd obj-primehub_test && ../../asp3/configure.rb -T primehub_gcc -L ../obj-primehub_kernel -a ../../test/ -A test -m ../../common/Makefile.appl)
 
 (cd obj-primehub_kernel && make libkernel.a -j $JOB_NUM)
-(cd obj-primehub_test && make -j $JOB_NUM && make asp.bin -j $JOB_NUM)
+(cd obj-primehub_test && make libpybricks.a -j $JOB_NUM && make -j $JOB_NUM && make asp.bin -j $JOB_NUM)
 
 cd ..
