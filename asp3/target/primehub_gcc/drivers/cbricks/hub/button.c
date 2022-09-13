119e63ea711c193e64a271db08c3373b43e3107a
Makoto Shimojima <shimojima_makoto@nias.ac.jp>
2022-09-14 00:44:06 +0900

    feat(cbricks/hub/button): add API for the hub built-in buttons.
    - implemented hub_button_is_pressed().
    - defined hub_button_t.

---
+++ asp3/target/primehub_gcc/drivers/cbricks/hub/button.c
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+/*
+ * API for the hub built-in button.
+ *
+ * Copyright (c) 2022 Embedded and Real-Time Systems Laboratory,
+ *                    Graduate School of Information Science, Nagoya Univ., JAPAN
+ */
+
+#include <t_syslog.h>
+#include <cbricks/hub/button.h>
+
+#include <pbdrv/button.h>
+
+
+/*
+ * We implement button API by just wrapping functions in external/libpybricks/lib/pbio/drv/button/button_gpio.c.
+ */
+
+void _hub_button_init(void) {
+  _pbdrv_button_init();
+}
+
+pbio_error_t hub_button_is_pressed(hub_button_t *pressed) {
+  return pbdrv_button_is_pressed(pressed);
+}
+
+
