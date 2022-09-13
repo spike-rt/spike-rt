fb9ebdca9621f8f0d5b222ec2a35af83773666bb
Makoto Shimojima <shimojima_makoto@nias.ac.jp>
2022-09-14 00:45:58 +0900

    feat(sample/button): add sample code for hub button.

---
+++ sample/button/button.c
@@ -0,0 +1,91 @@
+/*
+ *  TOPPERS/ASP Kernel
+ *      Toyohashi Open Platform for Embedded Real-Time Systems/
+ *      Advanced Standard Profile Kernel
+ * 
+ *  Copyright (C) 2000-2003 by Embedded and Real-Time Systems Laboratory
+ *                              Toyohashi Univ. of Technology, JAPAN
+ *  Copyright (C) 2004-2019 by Embedded and Real-Time Systems Laboratory
+ *              Graduate School of Information Science, Nagoya Univ., JAPAN
+ * 
+ *  上記著作権者は，以下の(1)〜(4)の条件を満たす場合に限り，本ソフトウェ
+ *  ア（本ソフトウェアを改変したものを含む．以下同じ）を使用・複製・改
+ *  変・再配布（以下，利用と呼ぶ）することを無償で許諾する．
+ *  (1) 本ソフトウェアをソースコードの形で利用する場合には，上記の著作
+ *      権表示，この利用条件および下記の無保証規定が，そのままの形でソー
+ *      スコード中に含まれていること．
+ *  (2) 本ソフトウェアを，ライブラリ形式など，他のソフトウェア開発に使
+ *      用できる形で再配布する場合には，再配布に伴うドキュメント（利用
+ *      者マニュアルなど）に，上記の著作権表示，この利用条件および下記
+ *      の無保証規定を掲載すること．
+ *  (3) 本ソフトウェアを，機器に組み込むなど，他のソフトウェア開発に使
+ *      用できない形で再配布する場合には，次のいずれかの条件を満たすこ
+ *      と．
+ *    (a) 再配布に伴うドキュメント（利用者マニュアルなど）に，上記の著
+ *        作権表示，この利用条件および下記の無保証規定を掲載すること．
+ *    (b) 再配布の形態を，別に定める方法によって，TOPPERSプロジェクトに
+ *        報告すること．
+ *  (4) 本ソフトウェアの利用により直接的または間接的に生じるいかなる損
+ *      害からも，上記著作権者およびTOPPERSプロジェクトを免責すること．
+ *      また，本ソフトウェアのユーザまたはエンドユーザからのいかなる理
+ *      由に基づく請求からも，上記著作権者およびTOPPERSプロジェクトを
+ *      免責すること．
+ * 
+ *  本ソフトウェアは，無保証で提供されているものである．上記著作権者お
+ *  よびTOPPERSプロジェクトは，本ソフトウェアに関して，特定の使用目的
+ *  に対する適合性も含めて，いかなる保証も行わない．また，本ソフトウェ
+ *  アの利用により直接的または間接的に生じたいかなる損害に関しても，そ
+ *  の責任を負わない．
+ * 
+ *  $Id: sample1.c 1237 2019-07-10 01:00:30Z ertl-hiro $
+ */
+
+#include <t_syslog.h>
+#include "kernel_cfg.h"
+#include "button.h"
+#include "cbricks/hub/button.h"
+
+//#include <pbsys/user_program.h>
+
+static inline hub_button_t hub_buttons_pressed(void)
+{
+  hub_button_t pressed;
+  hub_button_is_pressed(&pressed);
+  return pressed;
+}
+
+static hub_button_t wait_for_hub_buttons(hub_button_t buttons_to_watch)
+{
+  hub_button_t buttons = 0;
+  hub_button_t pressed;
+  while ((hub_buttons_pressed() & buttons_to_watch) == 0) dly_tsk(10000);
+  while (pressed = hub_buttons_pressed() & buttons_to_watch, pressed != 0) {
+    buttons |= pressed;
+    dly_tsk(10000);
+  }
+  return buttons;
+}
+
+/*
+ *  メインタスク
+ */
+#include <stdio.h>
+void
+main_task(intptr_t exinf)
+{
+//  pbsys_user_program_prepare(NULL); // pbsys_processをユーザプログラム実行状態に遷移させる．
+  dly_tsk(3000000);
+  printf("BUTTON\n");
+  while (1) {
+    SYSTIM st;
+    hub_button_t buttons = wait_for_hub_buttons(HUB_BUTTON_RIGHT|HUB_BUTTON_LEFT);
+    get_tim(&st);
+    printf("%u: %d ", (unsigned int) st, buttons);
+    if (buttons&HUB_BUTTON_LEFT)   printf("LEFT ");
+    if (buttons&HUB_BUTTON_RIGHT)  printf("RIGHT ");
+    if (buttons&HUB_BUTTON_CENTER) printf("CENTER ");
+    if (buttons&HUB_BUTTON_BT)     printf("BT ");
+    printf("\n");
+  }
+//  pbsys_user_program_unprepare();
+}
