88ebbf0f73c676ac3ebec30369d999395c279b72
SHU Yoshifumi <envzhu@gmail.com>
2023-01-25 01:30:14 +0900

    sample/motor: renew code and change to use USB Serial for syslog.

--- sample/motor/motor.c
+++ sample/motor/motor.c
@@ -1,69 +1,55 @@
-// SPDX-License-Identifier: MIT
 /*
- * Copyright (c) 2022 Embedded and Real-Time Systems Laboratory,
- *                    Graduate School of Information Science, Nagoya Univ., JAPAN
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2022-2023 Embedded and Real-Time Systems Laboratory,
+ *            Graduate School of Information Science, Nagoya Univ., JAPAN
  */
 
+#include <kernel.h>
+#include <kernel_cfg.h>
 #include <t_syslog.h>
-#include "kernel_cfg.h"
+#include <stdio.h>
+#include <serial/serial.h>
+#include <spike/hub/system.h>
+#include <spike/pup/motor.h>
+#include "app_config.h"
 #include "motor.h"
-#include "cbricks/pup/motor.h"
 
-//#include <pbsys/user_program.h>
 
 /*
- * Main Task
+ * Application Main Task
  */
-#include <stdio.h>
 void
 main_task(intptr_t exinf)
 {
-  // Prepare the pybricks runtime for running a user program.
-  // TODO: integrate pbsys_user_program_prepare() and wup_pybricks into one function. 
-  pbsys_user_program_prepare(NULL);
-  wup_pybricks();
-
   pbio_error_t err;
   pup_motor_t *motor;
   
-  dly_tsk(3000000);
-  // printf("Set Up Motor\n");
+  dly_tsk(3*1000*1000);
+  
+  // 以下のようにNewlib経由でシリアル入出力できる．
+  //extern FILE* serial_open_newlib_file(ID portid);
+  //FILE *fd = serial_open_newlib_file(SIO_USB_PORTID);
+  //fprintf(fd, "Set Up Motor\n");
+
+  syslog(LOG_NOTICE, "Set Up Motor\n");
 
   // Get pointer to servo
-  motor = pup_motor_get_device(PBIO_PORT_ID_A);
-  if(motor == NULL)
-  {
-
+  motor = pup_motor_get_device(PBIO_PORT_ID_MOTOR);
+  if(motor == NULL) {
+    hub_system_shutdown();
   }
   
-  // Set up servo
-  for(int i = 0; i < 10; i++)
-  {
-    bool reset_count = true;
-    err = pup_motor_setup(motor, PUP_DIRECTION_CLOCKWISE, reset_count);
-    if(err != PBIO_ERROR_AGAIN)
-      break;
-    
-    // Wait 1s and try one more
-    dly_tsk(1000000);
+  bool reset_count = true;
+  err = pup_motor_setup(motor, PUP_DIRECTION_CLOCKWISE, reset_count);
+  if(motor == NULL) {
+    hub_system_shutdown();
   }
-  // TEST_ASSERT_NOT_EQUAL(err, PBIO_ERROR_NO_DEV);
-  // TEST_ASSERT_EQUAL(err, PBIO_SUCCESS);
   
-  pup_motor_set_duty_limit(motor, 30);
-
-  err = pup_motor_set_speed(motor, 500);
-
-  if (err != PBIO_SUCCESS)
-  {
+  err = pup_motor_set_speed(motor, 300);
+  if (err != PBIO_SUCCESS) {
+    hub_system_shutdown();
   }
- 
+  dly_tsk(3*1000*1000);
 
-  while (1)
-  {
-    slp_tsk();
-  }
-
-  pbsys_user_program_unprepare();
-  wup_pybricks();
+  hub_system_shutdown();
 }
--- sample/motor/motor.h
+++ sample/motor/motor.h
@@ -1,38 +1,32 @@
-// SPDX-License-Identifier: MIT
 /*
+ * SPDX-License-Identifier: MIT
  * Copyright (c) 2022 Embedded and Real-Time Systems Laboratory,
  *                    Graduate School of Information Science, Nagoya Univ., JAPAN
  */
 
-/*
- *		サンプルプログラム(1)のヘッダファイル
- */
-
+#ifndef __MOTOR_H__
+#define __MOTOR_H__
 #include <kernel.h>
 
 /*
  *  タスクの優先度の定義
  */
 
-#define MAIN_PRIORITY	5		/* メインタスクの優先度 */
+#define MAIN_PRIORITY   5    /* メインタスクの優先度 */
 
 /*
  *  ターゲットに依存する可能性のある定数の定義
  */
 
-#ifndef TASK_PORTID
-#define	TASK_PORTID		1			/* 文字入力するシリアルポートID */
-#endif /* TASK_PORTID */
-
-#ifndef STACK_SIZE
-#define	STACK_SIZE		4096		/* タスクのスタックサイズ */
-#endif /* STACK_SIZE */
+#define MAIN_STACK_SIZE 4096    /* タスクのスタックサイズ */
 
 /*
  *  関数のプロトタイプ宣言
  */
 #ifndef TOPPERS_MACRO_ONLY
 
-extern void	main_task(intptr_t exinf);
+extern void  main_task(intptr_t exinf);
 
 #endif /* TOPPERS_MACRO_ONLY */
+
+#endif /* __MOTOR_H__ */
