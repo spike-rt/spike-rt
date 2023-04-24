6f061a0086fb796956462d9807bf9204aac48625
Makoto Shimojima <shimojima_makoto@nias.ac.jp>
2023-04-24 22:44:19 +0900

    feat(hub/speaker): add hub_speaker_play_tone(), hub_speaker_set_volume(), hub_speaker_stop()

---
+++ drivers/spike/hub/speaker.c
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+/*
+ * API for the hub built-in button.
+ *
+ * Copyright (c) 2022 Embedded and Real-Time Systems Laboratory,
+ *                    Graduate School of Information Science, Nagoya Univ., JAPAN
+ */
+
+#include <kernel.h>
+#include <spike/hub/speaker.h>
+#include <pbdrv/sound.h>
+#include <stdint.h>
+#include <math.h>
+
+#define WAVEFORM_LENGTH 128
+static uint16_t waveform_data[WAVEFORM_LENGTH];
+static uint8_t  waveform_volume = 0;
+
+void hub_speaker_set_volume(uint8_t volume)
+{
+  if (volume == waveform_volume) return;
+  uint16_t C = INT16_MAX;
+  uint16_t A = (pow(10, volume / 100.0) - 1) / 9 * INT16_MAX;
+  for (int i = 0; i < WAVEFORM_LENGTH; i++) {
+    waveform_data[i] = C + A * sin(M_PI * 2.0 * i / WAVEFORM_LENGTH); 
+  }
+}
+
+void hub_speaker_play_tone(uint16_t frequency, int32_t duration)
+{
+  if (frequency <    64) frequency =    64;
+  if (frequency > 24000) frequency = 24000;
+  pbdrv_sound_start(waveform_data, WAVEFORM_LENGTH, WAVEFORM_LENGTH * frequency / 2);
+  if (duration < 0) return;
+  dly_tsk(duration*1000);
+  hub_speaker_stop();
+}
+
+void hub_speaker_stop(void)
+{
+  pbdrv_sound_stop();
+}
---
+++ test/hub/test_speaker.c
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Tests for the internal buttons.
+ *
+ * Copyright (c) 2022 Embedded and Real-Time Systems Laboratory,
+ *                    Graduate School of Information Science, Nagoya Univ., JAPAN
+ */
+
+#include <kernel.h>
+
+#include <unity.h>
+#include <unity_fixture.h>
+
+#include <spike/hub/speaker.h>
+
+TEST_GROUP(Speaker);
+
+TEST_GROUP_RUNNER(Speaker) {
+  RUN_TEST_CASE(Speaker, beep);
+}
+
+TEST_SETUP(Speaker)
+{
+  // Prepare the pybricks runtime for running a user program.
+  // pbsys_user_program_prepare(NULL);
+}
+
+TEST_TEAR_DOWN(Speaker)
+{
+  // Perform cleanup/reset after running a user program.
+  // pbsys_user_program_unprepare();
+}
+
+TEST(Speaker, beep)
+{
+  hub_speaker_set_volume(100);
+  hub_speaker_play_tone(NOTE_C5, 200); dly_tsk(800000);
+  hub_speaker_play_tone(NOTE_C5, 200); dly_tsk(800000);
+  hub_speaker_play_tone(NOTE_C6, SOUND_MANUAL_STOP);
+  dly_tsk(1000000);
+  hub_speaker_stop();
+}
