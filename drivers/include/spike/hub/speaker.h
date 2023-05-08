14eac4b3ff878477076426baa2e3a7414fb27638
SHU Yoshifumi <densikousakusyosinsya@gmail.com>
2023-05-08 09:35:11 +0900

    Update drivers/include/spike/hub/speaker.h

--- drivers/include/spike/hub/speaker.h
+++ drivers/include/spike/hub/speaker.h
@@ -2,7 +2,7 @@
 /*
  * API for the hub built-in speaker.
  *
- * Copyright (c) 2022 Embedded and Real-Time Systems Laboratory,
+ * Copyright (c) 2023 Embedded and Real-Time Systems Laboratory,
  *                    Graduate School of Information Science, Nagoya Univ., JAPAN
  */
 
