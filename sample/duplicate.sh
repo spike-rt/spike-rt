dd293a0942d77c04b8581fb8756f6b8e5db00b30
Makoto Shimojima <shimojima_makoto@nias.ac.jp>
2023-10-26 15:57:20 +0900

    Fix: rename only the .h file

--- sample/duplicate.sh
+++ sample/duplicate.sh
@@ -5,9 +5,9 @@ mkdir -p ${target}
 
 (
   cd ${target}
-  sed s/${source}/${target}/g ../${source}/${source}.c    > ./${target}.c
-  sed s/${source}/${target}/g ../${source}/${source}.cdl  > ./${target}.cdl
-  sed s/${source}/${target}/g ../${source}/${source}.cfg  > ./${target}.cfg
-  sed s/${source}/${target}/g ../${source}/${source}.h    > ./${target}.h
+  sed s/${source}.h/${target}.h/g ../${source}/${source}.c    > ./${target}.c
+  sed s/${source}.h/${target}.h/g ../${source}/${source}.cdl  > ./${target}.cdl
+  sed s/${source}.h/${target}.h/g ../${source}/${source}.cfg  > ./${target}.cfg
+  sed s/${source}.h/${target}.h/g ../${source}/${source}.h    > ./${target}.h
   . ../config.sh ${target}
 )
