ef7154e5cd7053587f56e2975647b13a247cc7ab
exhonda <honda@ertl.jp>
2024-06-08 21:20:50 +0900

    不要なファイルの削除

--- sample/button/config.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p build
-( cd build
-  ../../../asp3/configure.rb -T primehub_gcc -L ../../../build/obj-primehub_kernel -a ../ -A ${1:-app} -m ../../../common/app.mk
-)
--- sample/button/usb_serial.c
+++
@@ -1,9 +0,0 @@
-// Output characters via spike-rt/external/libpybricks/lib/pbio/drv/usb/stm32_usb_serial.c
-// Add "APPL_COBJS += usb_serial.o" in Makefile.inc
-#include <stdbool.h>
-#include <stdint.h>
-bool pbsys_usb_stdout_put_char(uint8_t c);
-void low_putchar(char c)
-{
-	pbsys_usb_stdout_put_char(c);
-}
