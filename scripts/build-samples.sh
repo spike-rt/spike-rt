0e0c8ddf82e35b641520526b1d22c00f53d700b1
SHU Yoshifumi <envzhu@gmail.com>
2024-06-15 00:34:13 +0900

    scripts: add script to build all sample applications.

---
+++ scripts/build-samples.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -xe
+JOB_NUM=${JOB_NUM:-$(nproc)}
+
+CUR_DIR=$(pwd)
+THIS_SCRIPT_DIR=$(cd $(dirname $0);pwd)
+SR_TOP=$(cd $THIS_SCRIPT_DIR/../; pwd)
+BUILD_TOP=${BUILD_TOP:-"$SR_TOP/build"}
+
+
+mkdir -p $BUILD_TOP
+BUILD_TOP=$(cd $BUILD_TOP; pwd)
+
+pushd $BUILD_TOP
+
+mkdir -p obj-primehub_kernel
+pushd obj-primehub_kernel 
+$SR_TOP/asp3/configure.rb -T primehub_gcc -f -m $SR_TOP/common/kernel.mk
+
+# Currently, build libpybricks.a, first of all.
+# libkernel.a itself does not depend on libpybricks.a, 
+# but some codes in libkernel.a depend on header files which is cloned by `make libpybricks.a`. 
+make libpybricks.a -j $JOB_NUM 
+make libkernel.a -j $JOB_NUM
+
+popd
+
+
+# Build each application
+for APP in \
+    "button" "led" "motor"
+do
+  mkdir -p obj-primehub_$APP
+  pushd obj-primehub_$APP
+  $SR_TOP/asp3/configure.rb -T primehub_gcc -L $BUILD_TOP/obj-primehub_kernel -a $SR_TOP/sample/$APP -A $APP -m $SR_TOP/common/app.mk
+  make -j $JOB_NUM && make asp.bin -j $JOB_NUM
+  popd
+done
+
+popd
