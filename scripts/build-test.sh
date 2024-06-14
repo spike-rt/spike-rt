f474ee60878e90051c93101c528aa8d5fc190184
SHU Yoshifumi <envzhu@gmail.com>
2024-06-15 00:50:14 +0900

    scripts: refactor build-test.sh according to build-samples.sh.

--- scripts/build-test.sh
+++ scripts/build-test.sh
@@ -1,21 +1,34 @@
 #!/bin/bash -xe
-BUILD_DIR=${BUILD_DIR:-build}
 JOB_NUM=${JOB_NUM:-$(nproc)}
 
-mkdir -p $BUILD_DIR
-cd $BUILD_DIR
+CUR_DIR=$(pwd)
+THIS_SCRIPT_DIR=$(cd $(dirname $0);pwd)
+SR_TOP=$(cd $THIS_SCRIPT_DIR/../; pwd)
+BUILD_TOP=${BUILD_TOP:-"$SR_TOP/build"}
+
+
+mkdir -p $BUILD_TOP
+BUILD_TOP=$(cd $BUILD_TOP; pwd)
+
+pushd $BUILD_TOP
 
 mkdir -p obj-primehub_kernel
-(cd obj-primehub_kernel && ../../asp3/configure.rb -T primehub_gcc -f -m ../../common/kernel.mk)
-
-mkdir -p obj-primehub_test
-(cd obj-primehub_test && ../../asp3/configure.rb -T primehub_gcc -L ../obj-primehub_kernel -a ../../test/ -A test -m ../../common/app.mk)
+pushd obj-primehub_kernel 
+$SR_TOP/asp3/configure.rb -T primehub_gcc -f -m $SR_TOP/common/kernel.mk
 
 # Currently, build libpybricks.a, first of all.
 # libkernel.a itself does not depend on libpybricks.a, 
 # but some codes in libkernel.a depend on header files which is cloned by `make libpybricks.a`. 
-(cd obj-primehub_kernel && make libpybricks.a -j $JOB_NUM && make libkernel.a -j $JOB_NUM)
+make libpybricks.a -j $JOB_NUM 
+make libkernel.a -j $JOB_NUM
 
-(cd obj-primehub_test && make -j $JOB_NUM && make asp.bin -j $JOB_NUM)
+popd
 
-cd ..
+
+mkdir -p obj-primehub_test
+pushd obj-primehub_test
+$SR_TOP/asp3/configure.rb -T primehub_gcc -L $BUILD_TOP/obj-primehub_kernel -a $SR_TOP/test -A test -m $SR_TOP/common/app.mk
+make -j $JOB_NUM && make asp.bin -j $JOB_NUM
+popd
+
+popd
